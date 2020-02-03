9f0a60f0c7f4a43a672a2c51b4ba29738ab4c6ae
Davi Luis <daviluis323@gmail.com>
2020-02-03 04:40:48 -0300

    Add Advanced Search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def search_for_select
+    [
+      ['Fabricante', 'manufacturer'],
+      ['Material', 'structure_type'],
+      ['Kwp', 'kwp'],
+      ['Preco', 'price'],
+    ]
+  end
 end
--- app/services/power_generator_service.rb
+++ app/services/power_generator_service.rb
@@ -7,13 +7,26 @@ class PowerGeneratorService
 
   def all_power_generators
     @search = params['search']
+    @search_for_select = params['search_for_select']
+    @keyword = params['keyword']
     @power_generators = PowerGenerator.all
-    full_search if @search.present?
+    simple_search if @search.present?
+    if  @search_for_select.present?
+      @keyword = params['keyword']
+      advanced_search
+    end
     @power_generators = Kaminari.paginate_array(@power_generators).page(params[:page]).per(6)
   end
 
-  def full_search
+  def simple_search
     @power_generators = PowerGenerator.all.where('name LIKE :search or description LIKE :search', search: @search)
+  end
+
+  def advanced_search
+    @power_generators =  @power_generators.where(manufacturer: params[:keyword]) if params[:search_for_select] == 'manufacturer'
+    @power_generators =  @power_generators.where(structure_type: params[:keyword]) if params[:search_for_select] == 'structure_type'
+    @power_generators =  @power_generators.where(kwp: params[:keyword]) if params[:search_for_select] == 'kwp'
+    @power_generators =  @power_generators.where(price: params[:keyword]) if params[:search_for_select] == 'price'
   end
 
   private
